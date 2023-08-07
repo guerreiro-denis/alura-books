94420419f4cbc31fb37449d33f2d894601b2c7ca
Denis Rodrigues <89110188+guerreiro-denis@users.noreply.github.com>
2023-08-07 10:04:43 -0300

    Adicionando toda a parte de funcionalidade da aplicação
    Os arquivos de funcionalidade da aplicação onde o treinamento foi de fato realizado.

---
+++ app/metodoForEach.js
@@ -0,0 +1,26 @@
+const elementoLivros = document.getElementById('livros');
+const elementoValorTotalLivrosDisponiveis = document.getElementById("valor_total_livros_disponiveis");
+
+function exibirLivroNaTela(listaDeLivros) {
+  
+  elementoValorTotalLivrosDisponiveis.innerHTML = "";
+  elementoLivros.innerHTML = "";
+    listaDeLivros.forEach(livro =>{
+
+      let disponibilidade = livro.quantidade > 0 ? "livro__imagens" : "livro__imagens indisponivel";
+      
+      elementoLivros.innerHTML += `
+        <div class="livro">
+      <img class="${disponibilidade}" src="${livro.imagem}"
+        alt="${livro.alt}" />
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+      <p class="livro__descricao">${livro.autor}</p>
+      <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>`
+    });
+}
---
+++ app/metodoMap.js
@@ -0,0 +1,10 @@
+function aplicaDescontos (livros) {
+    const desconto = 0.3;
+
+    livrosComDesconto = livros.map( livro => {
+        
+        return {...livro, preco: livro.preco - (livro.preco * desconto)}
+        
+    });
+    return livrosComDesconto;
+}
